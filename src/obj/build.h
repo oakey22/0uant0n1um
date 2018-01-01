8c41274f2dbb01524e060e4865a24bc4a1d6d030
root <quantum@quantum.com>
2018-01-01 16:36:19 +0100

    first commit

---
+++ src/obj/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2017-12-31 15:08:41 +0100"
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Quantonium-Qt message queue name
+#define QuantoniumURI_QUEUE_NAME "QuantoniumURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
